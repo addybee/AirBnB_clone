148f64e66a0e02afd18dd964d04e8481a7f0d565
akinbode adeboyejo <akinbode.adeboyejo@gmail.com>
2024-03-05 11:35:43 -0800

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+timedate = __import__("timedate").datetime
+import uuid
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(")
