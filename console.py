43a759e6220d26f05b829ab9b74df7a0e83eda3b
NecheHephzibah <neche.bless@gmail.com>
2024-03-06 10:23:49 -0800

    Console

--- console.py
+++ console.py
@@ -7,14 +7,34 @@ class HBNBCommand(cmd.Cmd):
     """Defines a command processor."""
     
     prompt = '(hbnb) '
+    file_path = 'file.json'
 
-    def do_create(self, name):
+    def do_create(self, args):
         """
         Creates a new instance of BaseModel, saves it(to the JSON)
         and prints the id.
         """
-        if not name:
+        if not args:
             print("** class name missing **")
+            return
+
+        try:
+            classname = args.strip()
+            from_file = __import__("__main__")
+            cls_name = getattr(from_file, classname)
+            obj = cls_name()
+
+            with open(self.file_path, 'r+') as f:
+                data = json.load(f)
+                obj_dict = obj.to_dict()
+                data[obj_dict['__class__'] + '.' + obj_dict['id']] = obj_dict
+                f.seek(0)
+                json.dump(data, f)
+
+            print(obj_dict['id'])
+
+        except AttributeError:
+            print("** class doesn't exist **")
 
     def do_EOF(self, line):
         """
