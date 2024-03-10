3dd25cfa3f47d26aea7453772692c79a75bf3013
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2024-03-10 13:27:23 -0700

    task 13 and 14

--- console.py
+++ console.py
@@ -68,13 +68,18 @@ class HBNBCommand(cmd.Cmd):
         name and id
         """
         args = args.split(" ")
+        argc = len(args)
+
+        if argc == 2 and search(r'^"*[\w-]*"$', args[1]):
+            args[1] = args[1].strip('"')
         if not args[0]:
             print("** class name missing **")
         elif args[0] not in class_dict.keys():
             print("** class doesn't exist **")
-        elif len(args) < 2:
+        elif len(args) == 2 and not args[1]:
             print("** instance id missing **")
         else:
+
             key = ".".join(args)
             db = storage.all()
             if key in db.keys():
@@ -87,6 +92,10 @@ class HBNBCommand(cmd.Cmd):
         Deletes an instance based on the class name and id
         """
         args = args.split(" ")
+        argc = len(args)
+
+        if argc == 2 and search(r'^"*[\w-]*"$', args[1]):
+            args[1] = args[1].strip('"')
         if not args[0]:
             print("** class name missing **")
         elif args[0] not in class_dict.keys():
@@ -176,7 +185,9 @@ class HBNBCommand(cmd.Cmd):
     def default(self, args):
         """ handles undefined commands """
         argv = args.split(".")
-        if len(argv) == 2:
+        argc = len(argv)
+
+        if argc == 2:
             if argv[0] in class_dict.keys():
                 if argv[1] == "all()":
                     self.do_all(argv[0])
@@ -184,6 +195,28 @@ class HBNBCommand(cmd.Cmd):
                 elif argv[1] == "count()":
                     self.do_count(argv[0])
                     return
+                elif search(r'^show\("[\w-]*"\)$', argv[1]):
+                    id = argv[1].strip('show(').strip(')')
+                    name_id = "{} {}".format(argv[0], id)
+                    self.do_show(name_id)
+                    return
+                elif search(r'^destroy\("[\w-]*"\)$', argv[1]):
+                    id = argv[1].strip('destroy(').strip(')')
+                    name_id = "{} {}".format(argv[0], id)
+                    self.do_destroy(name_id)
+                    return
+            else:
+                if search(r'^show\("[\w-]*"\)$', argv[1]):
+                    id = argv[1].strip('show("').strip('")')
+                    name_id = "{} {}".format(argv[0], id)
+                    self.do_show(name_id)
+                    return
+                elif search(r'^destroy\("[\w-]*"\)$', argv[1]):
+                    id = argv[1].strip('destroy(').strip(')')
+                    name_id = "{} {}".format(argv[0], id)
+                    self.do_destroy(name_id)
+                    return
+
         print('*** Unknown syntax: {}'.format(args))
 
 
